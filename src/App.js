7e5e2b435ba6a19d029e08489ed283ca0d135681
Dave <drregg6@gmail.com>
2019-02-22 19:36:49 -0500

    removed state arr, starting api portion

--- src/App.js
+++ src/App.js
@@ -12,23 +12,7 @@ import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: 1,
-                title: 'Take out the trash',
-                completed: false
-            },
-            {
-                id: 2,
-                title: 'Dinner with wife',
-                completed: false
-            },
-            {
-                id: 3,
-                title: 'Meeting with boss',
-                completed: false
-            }
-        ]
+        todos: []
     }
     
     markComplete = (id) => {
